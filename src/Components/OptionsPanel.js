ae210a45304cc64216b759d7f722d455494b2ce5
Felipe Sampaio <felipe.sampaio@farroupilha.ifrs.edu.br>
2024-02-08 15:00:37 -0300

    Basic picker to provide file upload at GUI

--- src/Components/OptionsPanel.js
+++ src/Components/OptionsPanel.js
@@ -1,7 +1,17 @@
 import styles from "./OptionsPanel.module.css"
 
 function OptionsPanel( {handleClearMemories, handleSaveMemories} ) {
-       
+    
+    const handleFileChange = async (e) => {
+        e.preventDefault()
+        const reader = new FileReader()
+        reader.onload = async (e) => { 
+            const text = (e.target.result)
+            console.log(text)
+        };
+        reader.readAsText(e.target.files[0])
+    }
+
     return (
         <div className = {styles.container}>
             <h1>Options</h1>
@@ -12,7 +22,7 @@ function OptionsPanel( {handleClearMemories, handleSaveMemories} ) {
                 <button onClick={handleSaveMemories}>
                     Save Memories
                 </button>
-
+                <input type="file" onChange={handleFileChange}/>
             </div>
         </div>
     )
